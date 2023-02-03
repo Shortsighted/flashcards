416d59c665377afc8bf1dfc661bd69c224bf61a5
Shortsighted <onlyforyoumyhoney@gmail.com>
2023-02-03 18:40:31 +0400

    Updated Collection section, added folder functionality.

--- src/WordCreator.js
+++ src/WordCreator.js
@@ -4,28 +4,32 @@ import styles from "./WordCreator.module.css"
 export default class WordCreator extends Component{
     render(){
         return(
-            <div className={styles.mainBox}>
-                <h2>Please, input the details below.</h2>
+            <>
+                {this.props.newWordStatus && 
+                <div className={styles.popup}>{this.props.newWordStatus}</div>}
+                <div className={styles.mainBox}>
+                    <h2>Please, input the details below.</h2>
 
-                <form className={styles.inputForm}
-                      onSubmit={this.props.onSubmit}
-                >
-                    <div className={styles.inputFields}>
-                        <div className={styles.labels}>
-                            <label htmlFor="mainWord">Main word:</label>
-                            <label htmlFor="translation">Translation:</label>
+                    <form className={styles.inputForm}
+                        onSubmit={this.props.onSubmit}
+                    >
+                        <div className={styles.inputFields}>
+                            <div className={styles.labels}>
+                                <label htmlFor="mainWord">Word:</label>
+                                <label htmlFor="meaning">Meaning:</label>
+                            </div>
+                            <div className={styles.words}>
+                                <input name="mainWord" required />
+                                <input name="meaning" required />
+                            </div>
                         </div>
-                        <div className={styles.words}>
-                            <input name="mainWord" required />
-                            <input name="translation" required />
-                        </div>
-                    </div>
-                    <button>
-                        <div>Add to List</div>
-                    </button>
-                </form>
-                <p onClick={this.props.viewDictionaryClick}>View current dictionary</p>
-            </div>
+                        <button>
+                            <div>Add to List</div>
+                        </button>
+                    </form>
+                    <p onClick={this.props.viewCollectionClick}>View your collection</p>
+                </div>
+            </>
         )
     }
 }
